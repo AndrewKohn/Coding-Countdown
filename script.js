fc2d00fb2cbcc7adf4d8e08b563fb76d7235ce5e
Andrew Kohn <87670203+AndrewKohn@users.noreply.github.com>
2022-01-15 16:56:19 -0600

    worked on countdown functionality

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+const startWindow = document.querySelector(`.start-window`);
+const countdownWindow = document.querySelector(`.countdown-window`);
+let hours, minutes, seconds;
+
+const countdownTimer = function () {};
+
+// START MENU
+// Countdown btn pressed
+document
+  .querySelector(`.btn--countdown`)
+  .addEventListener(`click`, function () {
+    document.querySelector(`.countdown-input`).classList.toggle('hidden');
+    document.querySelector(`.countdown-input`).focus();
+  });
+
+// Countdown inputs
+document
+  .querySelector(`.countdown-input`)
+  .addEventListener('keydown', function (userKey) {
+    if (userKey.key === 'Enter') {
+      const userInput = document.querySelector(`.countdown-input`).value;
+      hours = Number(userInput.split(':')[0]);
+      minutes = Number(userInput.split(':')[1]);
+
+      if (hours < 10) {
+        document.querySelector(`.countdown--hours`).textContent = `0` + hours;
+      } else {
+        document.querySelector(`.countdown--hours`).textContent = hours;
+      }
+
+      if (minutes < 10) {
+        document.querySelector(`.countdown--minutes`).textContent =
+          `0` + minutes;
+      } else {
+        document.querySelector(`.countdown--minutes`).textContent = minutes;
+      }
+
+      countdownTimer();
+
+      startWindow.classList.toggle('hidden');
+      countdownWindow.classList.toggle('hidden');
+    }
+  });
